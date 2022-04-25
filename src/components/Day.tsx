6f763b67d4a49e375f4bb956e6196308f61e2d40
kdm3578@gmail.com <kdm3578@gmail.com>
2022-04-25 23:54:15 +0900

    reset

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,15 +1,4 @@
-import {
-  DragDropContext,
-  Draggable,
-  Droppable,
-  DropResult,
-} from "react-beautiful-dnd";
-import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { WeekToDoState } from "../atoms";
-import ToDoBoard from "./ToDoBoard";
-import DragabbleCard from "./DragabbleCard";
-import { TODAY } from "../time";
 
 const Wrapper = styled.div`
   display: flex;
@@ -29,48 +18,7 @@ const Boards = styled.div`
 `;
 
 function Day() {
-  const [toDos, setToDos] = useRecoilState(WeekToDoState);
-  const onDragEnd = (info: DropResult) => {
-    const { destination, draggableId, source } = info;
-    if (!destination) return;
-    if (destination?.droppableId === source.droppableId) {
-      //same board movement
-      setToDos((allBoards) => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: boardCopy,
-        };
-      });
-    }
-    if (destination.droppableId !== source.droppableId) {
-      //cross board movement
-      setToDos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-  };
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Wrapper>
-        <Boards>
-          <ToDoBoard boardId={TODAY} toDos={toDos[TODAY]}></ToDoBoard>
-        </Boards>
-      </Wrapper>
-    </DragDropContext>
-  );
+  return <div>Day</div>;
 }
 
 export default Day;
